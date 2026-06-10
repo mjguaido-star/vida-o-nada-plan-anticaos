5bf64f983fa3722c4ee095b97df45837d0616cf1
Marian Guaido Paris <marianguaidoparis@Marians-MacBook-Air.local>
2026-06-10 22:57:39 +0100

    Pausa: Leica del usuario como camara de perfil

--- sections.js
+++ sections.js
@@ -856,7 +856,7 @@ function MomentoIncomodo() {
     className: "camera-section"
   }, React.createElement("img", {
     className: "camera-img",
-    src: "./assets/camara-lado.png",
+    src: "./assets/camara-leica.png",
     alt: "",
     "aria-hidden": "true"
   }), React.createElement("div", {
